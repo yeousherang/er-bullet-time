588587207524f81d8fe4bacef8bfb7b1064de624
yeousherang <yeousherang@proton.me>
2026-07-25 02:28:52 +0900

    apply fromsoftware-rs tutorial

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,51 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::time::Duration;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use eldenring::{
+    cs::{CSTaskGroupIndex, CSTaskImp, ChrInsExt, WorldChrMan},
+    fd4::FD4TaskData,
+    util::input,
+};
+use fromsoftware_shared::{FromStatic, SharedTaskImpExt};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+const SP_EFFECT: i32 = 4330;
+
+/// # Safety
+/// This is exposed this way such that libraryloader can call it. Do not call this yourself.
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn DllMain(_hmodule: u64, reason: u32) -> bool {
+    // Exit early if we're not attaching a DLL
+    if reason != 1 {
+        return true;
     }
+
+    std::thread::spawn(move || {
+        // Retrieve games task runner and register a task at frame begin.
+        let cs_task = CSTaskImp::wait_for_instance(Duration::MAX).unwrap();
+        cs_task.run_recurring(
+            |_: &FD4TaskData| {
+                // Retrieve WorldChrMan
+                let Ok(world_chr_man) = (unsafe { WorldChrMan::instance_mut() }) else {
+                    return;
+                };
+
+                // Retrieve main player
+                let Some(ref mut main_player) = world_chr_man.main_player else {
+                    return;
+                };
+
+                // Check if "o" is pressed
+                if input::is_key_pressed(0x4F) {
+                    main_player.apply_speffect(SP_EFFECT, true);
+                }
+
+                // Check if "p" is pressed
+                if input::is_key_pressed(0x50) {
+                    main_player.chr_ins.remove_speffect(SP_EFFECT);
+                }
+            },
+            CSTaskGroupIndex::FrameBegin,
+        );
+    });
+
+    true
 }
