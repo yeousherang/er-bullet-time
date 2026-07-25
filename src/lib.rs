e48a869a7c217153dcd6282fcbd01f218d158f82
yeousherang <yeousherang@proton.me>
2026-07-25 18:27:00 +0900

    add bullet time toggle with animation speed control

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,28 @@ use eldenring::{
     fd4::FD4TaskData,
     util::input,
 };
+use eldenring::cs::ChrType;
 use fromsoftware_shared::{FromStatic, SharedTaskImpExt};
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 const SP_EFFECT: i32 = 4330;
+const NORMAL_SPEED: f32 = 1.0;
+const TARGET_SPEED: f32 = 0.0;
+const BULLET_TIME_KEY: i32 = 0x40;
+
+enum ActionType {
+    Toggle = 0,
+    Hold
+}
+
+enum KeyState {
+    Idle,
+    Holding,
+    Press,
+    Release,
+}
+
 
 static LOG_GUARD: OnceLock<WorkerGuard> = OnceLock::new();
 
@@ -73,12 +90,48 @@ pub unsafe extern "C" fn DllMain(_hmodule: u64, reason: u32) -> bool {
                     // log
                     tracing::info!("o pressed");
                     main_player.apply_speffect(SP_EFFECT, true);
+
+                    for chr_set_opt in &world_chr_man.chr_sets {
+                        if let Some(chr_set) = chr_set_opt {
+                            for chr in chr_set.characters() {
+                                if chr as *const _ as usize  == 0 {
+                                    continue;
+                                }
+
+                                let _ = std::panic::catch_unwind(std::panic::AssertUnwindSafe(move || {
+                                    chr.modules.behavior.animation_speed = if chr.chr_type == ChrType::Local {
+                                        NORMAL_SPEED
+                                    } else {
+                                        TARGET_SPEED
+                                    };
+                                }));
+                            }
+                        }
+                    }
                 }
 
                 // Check if "p" is pressed
                 if input::is_key_pressed(0x50) {
                     tracing::info!("p pressed");
                     main_player.chr_ins.remove_speffect(SP_EFFECT);
+
+                    for chr_set_opt in &world_chr_man.chr_sets {
+                        if let Some(chr_set) = chr_set_opt {
+                            for chr in chr_set.characters() {
+                                if chr as *const _ as usize  == 0 {
+                                    continue;
+                                }
+
+                                let _ = std::panic::catch_unwind(std::panic::AssertUnwindSafe(move || {
+                                    chr.modules.behavior.animation_speed = if chr.chr_type == ChrType::Local {
+                                        NORMAL_SPEED
+                                    } else {
+                                        NORMAL_SPEED
+                                    };
+                                }));
+                            }
+                        }
+                    }
                 }
             },
             CSTaskGroupIndex::FrameBegin,
